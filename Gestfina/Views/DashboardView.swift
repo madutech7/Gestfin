d51e568e9baf1671ddfa62962bc1a8ed8d68884c
madutech7 <madutech7@gmail.com>
2026-05-11 19:49:09 +0000

    fix: removed duplicate CustomRoundedCorner struct causing build error

--- Gestfina/Views/DashboardView.swift
+++ Gestfina/Views/DashboardView.swift
@@ -19,16 +19,6 @@ struct CustomRoundedCorner: Shape {
     }
 }
 
-struct CustomRoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
-
 struct DashboardView: View {
     @EnvironmentObject var viewModel: FinanceViewModel
     @State private var showSettings   = false
